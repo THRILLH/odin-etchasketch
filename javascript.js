062c74c6ff0ba616a3af5890a5279ae428eddeea
Matt Seton <mmseton@gmail.com>
2024-08-26 11:33:50 -0300

    Correct function to draw div pixels for any number

--- javascript.js
+++ javascript.js
@@ -1,31 +1,16 @@
 const sketchContainer = document.querySelector("#sketch-container");
-
-function fillSquare() {
-    let dynamicVars = {};
-    for (let i = 1; i <= 9; i++) {
-        dynamicVars[`pix${i}`] = document.createElement("div");
+// Change function so parameter is width and number of 
+// pixels is width squared
+function fillSquare(pixels) {
+    let pixArray = [];
+    for (let i = 0; i < pixels; i++) {
+        pixArray[i] = document.createElement("div");
+        pixArray[i].classList.add("pixel");
+        sketchContainer.appendChild(pixArray[i]);
     }
-        dynamicVars.pix1.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix1)
-        dynamicVars.pix2.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix2)
-        dynamicVars.pix3.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix3)
-        dynamicVars.pix4.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix4)
-        dynamicVars.pix5.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix5)
-        dynamicVars.pix6.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix6)
-        dynamicVars.pix7.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix7)
-        dynamicVars.pix8.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix8)
-        dynamicVars.pix9.classList.add("pixel");
-        sketchContainer.appendChild(dynamicVars.pix9)
     }
 
-fillSquare();
+fillSquare(256);
 
 
 
